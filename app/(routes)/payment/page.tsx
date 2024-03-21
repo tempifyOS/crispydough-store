6a5e5615271dd672c9e096e0626e0578c0fb3d6f
Ryan A. Hunt <ryanahunt@outlook.com>
2024-03-20 23:15:23 -0500

    feat: payment page for safety

---
+++ app/(routes)/payment/page.tsx
@@ -0,0 +1,25 @@
+import Container from "@/components/ui/container";
+
+const PaymentSafetyPage = () => {
+  return (
+    <Container>
+      <div className="py-10 px-4 sm:px-6 lg:px-8">
+        <h1 className="text-3xl font-extrabold text-gray-900 mb-4">Payment Safety</h1>
+        <p className="text-lg text-gray-700 mb-6">
+          At <a href="/" className="text-blue-500 underline">Crispy Dough Bakery</a>, we take your payment security seriously. That's why we utilize Stripe, a trusted and secure payment processing platform, to handle all transactions on our website.
+        </p>
+        <p className="text-lg text-gray-700 mb-6">
+          <a className="text-blue-500 underline" href="https://docs.stripe.com/security" target="blank">Stripe</a> employs industry-leading security measures to ensure that your payment information is protected. When you make a purchase with us, you can rest assured that your sensitive data is encrypted and safeguarded.
+        </p>
+        <p className="text-lg text-gray-700 mb-6">
+          Additionally, <a className="text-blue-500 underline" href="https://docs.stripe.com/security" target="blank">Stripe</a> is certified as a PCI Level 1 Service Provider, the highest level of certification available in the payment industry. This means that they adhere to the most stringent security standards to protect your financial information.
+        </p>
+        <p className="text-lg text-gray-700 mb-6">
+          Your trust and confidence in our payment process are paramount to us. If you have any questions or concerns about payment safety or our use of Stripe, please don't hesitate to <a href="/contact" className="text-blue-500 underline">contact us</a>.
+        </p>
+      </div>
+    </Container>
+  );
+};
+
+export default PaymentSafetyPage;
--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,19 +1,22 @@
+// MainNav.tsx
 "use client";
-
-import Link from "next/link"
+import { useState } from "react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 import { Category } from "@/types";
 
 interface MainNavProps {
   data: Category[];
 }
 
-const MainNav: React.FC<MainNavProps> = ({
-  data
-}) => {
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   const routes = data.map((route) => ({
     href: `/category/${route.id}`,
@@ -21,8 +24,9 @@ const MainNav: React.FC<MainNavProps> = ({
     active: pathname === `/category/${route.id}`,
   }));
 
-  const aboutUsRoute = routes.find(route => route.href === "/about");
-  
+  const aboutUsRoute = routes.find((route) => route.href === "/about");
+  const paymentRoute = routes.find((route) => route.href === "/payment");
+
   // If "About Us" link is not present, add it to routes
   if (!aboutUsRoute) {
     routes.push({
@@ -32,24 +36,58 @@ const MainNav: React.FC<MainNavProps> = ({
     });
   }
 
+  if (!paymentRoute) {
+    routes.push({
+      href: "/payment",
+      label: "Payments",
+      active: pathname === "/payment",
+    });
+  }
+
   return (
-    <nav
-      className="mx-6 flex items-center space-x-4 lg:space-x-6"
-    >
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-black',
-            route.active ? 'text-black' : 'text-neutral-500'
-          )}
+    <nav className="flex flex-col lg:flex-row items-center lg:items-stretch">
+      <div className="lg:hidden">
+        <button
+          onClick={toggleMenu}
+          className="text-sm font-medium transition-colors hover:text-black"
         >
-          {route.label}
-      </Link>
-      ))}
+          {/* Hamburger menu icon */}
+          <svg
+            className="mr-2 h-6 w-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } lg:flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 lg:space-x-4 ml-3 gap-1`}
+      >
+        {routes.map((route) => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={cn(
+              "text-sm font-medium transition-colors hover:text-black",
+              route.active ? "text-black" : "text-neutral-500"
+            )}
+          >
+            {route.label}
+          </Link>
+        ))}
+      </div>
     </nav>
-  )
+  );
 };
 
 export default MainNav;
