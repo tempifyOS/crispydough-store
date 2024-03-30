60f445fb453af8929b445242f61816da80b65437
Ryan A. Hunt <ryanahunt@outlook.com>
2024-03-30 01:19:33 -0500

    fix: product card

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -38,24 +38,24 @@ const ProductCard: React.FC<ProductCard> = ({
     cart.addItem(data);
   };
   
-  return ( 
+  return (
     <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
       {/* Image & actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
-        <Image 
-          src={data.images?.[0]?.url} 
-          alt="" 
+        <Image
+          src={data.images?.[0]?.url}
+          alt=""
           fill
           className="aspect-square object-cover rounded-md"
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton 
-              onClick={onPreview} 
+            <IconButton
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={onAddToCart} 
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />} 
             />
           </div>
