679ca307339379a1f6f21a0f1a5f98cd587bef4c
Ryan A. Hunt <ryanahunt@outlook.com>
2024-03-30 01:18:11 -0500

    fix: main nav overlapping

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,4 +1,3 @@
-// MainNav.tsx
 "use client";
 import { useState } from "react";
 import Link from "next/link";
@@ -32,7 +31,6 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const paymentRoute = routes.find((route) => route.href === "/payment");
   const contactRoute = routes.find((route) => route.href === "/contact");
 
-  // If "About Us" link is not present, add it to routes
   if (!aboutUsRoute) {
     routes.push({
       href: "/about",
@@ -57,6 +55,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
     });
   }
 
+  // Adjusted classNames for better control of layout based on the open state
   return (
     <nav className="ml-3 flex flex-col lg:flex-row items-center lg:items-stretch">
       <div className="lg:hidden">
@@ -64,7 +63,6 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
           onClick={toggleMenu}
           className="text-sm font-medium transition-colors hover:text-black"
         >
-          {/* Hamburger menu icon */}
           <svg
             className="mr-2 h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -72,25 +70,19 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
           </svg>
         </button>
       </div>
       <div
         className={`${
-          isOpen ? "left-0" : "-left-full"
-        } lg:static lg:flex lg:flex-row lg:items-center lg:space-x-4 lg:p-0 lg:ml-auto lg:w-auto lg:bg-transparent lg:shadow-none lg:transition-none lg:duration-0 lg:transform-none lg:space-y-0 fixed top-0 h-screen w-3/4 bg-white z-20 flex flex-col py-8 px-6`}
+          isOpen ? "fixed" : "hidden"
+        } lg:static lg:flex lg:flex-row lg:items-center lg:space-x-4 lg:p-0 lg:ml-auto lg:w-auto lg:bg-transparent lg:shadow-none lg:visible lg:opacity-100 transition-all ease-in-out duration-300 top-0 left-0 h-screen w-3/4 bg-white z-50 lg:z-auto flex flex-col py-8 px-6`}
       >
         <button
           onClick={closeMenu}
           className="absolute top-3 right-3 lg:hidden text-gray-600"
         >
-          {/* Close menu icon */}
           <svg
             className="h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
@@ -98,12 +90,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
             viewBox="0 0 24 24"
             stroke="currentColor"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 18L18 6M6 6l12 12"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
         {routes.map((route) => (
@@ -111,7 +98,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
             key={route.href}
             href={route.href}
             active={route.active}
-            onClick={closeMenu} // Close the menu when clicking on a link
+            onClick={closeMenu}
           >
             {route.label}
           </LinkWrapper>
