26af9fa61c95c80e2882f190047562158e0d69d6
Ryan A. Hunt <ryanahunt@outlook.com>
2024-04-05 16:47:22 -0500

    remove: warning

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -27,7 +27,6 @@ const CartPage = () => {
       <Container>
         <div className="px-4 py-16 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
-          <h2 className='text-red-500 text-md font-bold'>NOTE: Currently the quantity feature is not working, in order to purchase multiple quantities of the same item, please make seperate orders for the time being. <br />  We apologize for the inconvenience.</h2>
           <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
             <div className="lg:col-span-7">
               {cart.items.length === 0 && <p className="text-neutral-500">No items added to cart.</p>}
